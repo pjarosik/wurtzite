f32de19ae3adebaf1f37950f248460f5a6017f8a
Piotr Jarosik <jarosik.piotr@gmail.com>
2023-08-01 19:09:50 +0200

    Created i/o examples.

--- crystalpy/io.py
+++ crystalpy/io.py
@@ -4,6 +4,7 @@ I/O functions, to support the most popular chemical formats.
 import os.path
 
 import numpy as np
+import sys
 from crystalpy.model import Crystal
 from openbabel import openbabel
 
@@ -12,14 +13,18 @@ def convert_from_openbabel(molecule: openbabel.OBMol) -> Crystal:
     atomic_numbers = []
     coordinates = []
     bonds = []
-    for atom in openbabel.OBMolAtomIter(molecule):
+    n_atoms = molecule.NumAtoms()
+    for i, atom in enumerate(openbabel.OBMolAtomIter(molecule)):
+        sys.stdout.write(f"\rReading atom: {(i+1)}/{n_atoms}")
         atomic_numbers.append(atom.GetAtomicNum())
         vec = atom.GetVector()
         coordinates.append(
             [vec.GetX(), vec.GetY(), vec.GetZ()]
         )
 
-    for bond in openbabel.OBMolBondIter(molecule):
+    n_bonds = molecule.NumBonds()
+    for i, bond in enumerate(openbabel.OBMolBondIter(molecule)):
+        sys.stdout.write(f"\rReading bond: {(i+1)}/{n_bonds}")
         bonds.append(
             [bond.GetBeginAtom().GetId(), bond.GetEndAtom().GetId()]
         )
@@ -39,7 +44,7 @@ def convert_to_openbabel(crystal: Crystal) -> openbabel.OBMol:
         ob_atom.SetVector(x, y, z)
     for bond in crystal.get_bonds():
         a, b = bond.a_id, bond.b_id
-        molecule.AddBond(a+1, b+1, 1)  # NOTE: atoms indexed from 1
+        molecule.AddBond(int(a)+1, int(b)+1, 1)  # NOTE: atoms indexed from 1
     return molecule
 
 
