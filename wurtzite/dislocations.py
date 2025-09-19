7ffafd43c599fd862e04c506c4eb56f6b70be413
Piotr Jarosik <jarosik.piotr@gmail.com>
2025-09-19 08:41:55 +0200

    Work in progress.

--- wurtzite/dislocations.py
+++ wurtzite/dislocations.py
@@ -54,12 +54,12 @@ def love_function(x: np.ndarray, be: float, bz: float) -> np.ndarray:
     r = np.sqrt(r2)
     x1_norm = x1/r
     x2_norm = x2/r
-    r02 = RADIUS_FACTOR*be**2
+    r02 = (RADIUS_FACTOR*be)**2
 
     ux = be/(2*np.pi)*(
         np.arctan2(x2_norm, x1_norm)
         + x1_norm*x2_norm/(2.0*(1-NU))
-    )
+    ) 
     uy = -be/(8*np.pi*(1-NU)) * (
         (1.0-NU-NU)*np.log(r2/r02)
         + (x1_norm+x2_norm)*(x1_norm-x2_norm)
